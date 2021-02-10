94caa4d865706bc46d3b8bdfe1ef3eadda4d24aa
AndyZhang2009 <andy20090210@outlook.com>
2021-02-10 12:22:15 +0800

    Update some file

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ScoreBasicTest
 ## 作者很懒，没写Readme
+### 作者：[AndyZhang2009](https://www.github.com/AndyZhang2009)
--- src/main/Main.java
+++ src/main/Main.java
@@ -10,24 +10,27 @@ import java.awt.event.WindowListener;
 import javax.swing.*;
 import java.util.Random;
 
+import static java.lang.Math.*;
+
 class SThread extends Thread {
 	@Override
     public void run() {
 		long st =  System.currentTimeMillis();
+		double a = 0;
 		for(int i = 1;i <= 1000*50000 ;i++) {
-			Math.cos(Main.r.nextDouble());
-			Math.sin(Main.r.nextDouble());
-			Math.log10(Main.r.nextDouble());
-			Math.tan(Main.r.nextDouble());
-			Math.cosh(Main.r.nextDouble());
-			Math.sinh(Main.r.nextDouble());
-			Math.tanh(Main.r.nextDouble());
+			a = cos(Main.r.nextDouble());
+			a = sin(Main.r.nextDouble());
+			a = log10(Main.r.nextDouble());
+			a = tan(Main.r.nextDouble());
+			a = cosh(Main.r.nextDouble());
+			a = sinh(Main.r.nextDouble());
+			a = tanh(Main.r.nextDouble());
 			Main.s.setText("跑分中..."+i);
 			Main.pb.setValue(i);
 		}
 		long et =  System.currentTimeMillis();
 		long time = et - st;
-		long ds = (long) Math.ceil((100 - time / 1000.0)*100);
+		long ds = (long) ceil((100 - time / 1000.0)*100);
 		Main.s.setText("分数:"+ds);
     }
 }
@@ -91,15 +94,15 @@ public class Main {
 		
 		try {
 			UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusStyle");
-		}catch(Exception ex) {
+		}catch(Exception ignored) {
 			
 		}
 
 		// TODO 自动生成的方法存根
 		mf.setSize(230, 175);
 		// 设置字体
-		s.setFont(new java.awt.Font("黑体", 1, 16));
-		start_btn.setFont(new java.awt.Font("黑体", 1, 21));
+		s.setFont(new java.awt.Font("黑体", Font.PLAIN, 16));
+		start_btn.setFont(new java.awt.Font("黑体", Font.PLAIN, 21));
 		// 按钮添加事件
 		start_btn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -107,8 +110,7 @@ public class Main {
 					Object[] options = {"确定","帮助"};
 					int res = JOptionPane.showOptionDialog(null, "请等待跑分完成","错误",JOptionPane.YES_NO_OPTION,JOptionPane.ERROR_MESSAGE,null,options,options[0]);
 					if(res != JOptionPane.YES_OPTION) {
-						JOptionPane.showMessageDialog(null, "1.分数最大是10000，但一般情况下不可能跑到10000分。\n"+
-																						   "2.没了", "帮助", JOptionPane.INFORMATION_MESSAGE);
+						JOptionPane.showMessageDialog(null, new StringBuilder().append("1.分数最大是10000，但一般情况下不可能跑到10000分。\n").append("2.没了").toString(), "帮助", JOptionPane.INFORMATION_MESSAGE);
 					}
 				}
 				else {
@@ -167,7 +169,7 @@ public class Main {
 			@Override
 			public void windowDeactivated(WindowEvent e) {
 				// TODO 自动生成的方法存根
-				
+
 			}
 			
 			@Override
